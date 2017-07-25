74d395f6695c6caaaf0578260209f5cd43c81b29
Fabian Hahn <fabian@improbable.io>
2017-07-25 21:48:40 +0100

    added shoveler integration for client

--- workers/shoveler/client/main.cpp
+++ workers/shoveler/client/main.cpp
@@ -1,18 +1,67 @@
 #include <improbable/worker.h>
 
-int main(int argc, char** argv) {
-  if (argc != 4) {
-    return 1;
-  }
+extern "C" {
+#include <camera/identity.h>
+#include <game.h>
+#include <log.h>
+}
 
-  worker::ConnectionParameters parameters;
-  parameters.WorkerType = "ShovelerClient";
-  parameters.Network.ConnectionType = worker::NetworkConnectionType::kTcp;
-  parameters.Network.UseExternalIp = false;
+static void updateGame(ShovelerGame *game, double dt);
 
-  const std::string workerId = argv[1];
-  const std::string hostname = argv[2];
-  const std::uint16_t port = static_cast<std::uint16_t>(std::stoi(argv[3]));
+int main(int argc, char **argv) {
+	if (argc != 4) {
+		return 1;
+	}
 
-  worker::Connection connection = worker::Connection::ConnectAsync(hostname, port, workerId, parameters).Get();
+	const char *windowTitle = "ShovelerClient";
+	bool fullscreen = false;
+	bool vsync = true;
+	int samples = 4;
+	int width = 640;
+	int height = 480;
+
+	ShovelerGame *game = shovelerGameCreate(windowTitle, width, height, samples, fullscreen, vsync);
+	if(game == NULL) {
+		return EXIT_FAILURE;
+	}
+
+	worker::ConnectionParameters parameters;
+	parameters.WorkerType = "ShovelerClient";
+	parameters.Network.ConnectionType = worker::NetworkConnectionType::kTcp;
+	parameters.Network.UseExternalIp = false;
+
+	const std::string workerId = argv[1];
+	const std::string hostname = argv[2];
+	const std::uint16_t port = static_cast<std::uint16_t>(std::stoi(argv[3]));
+
+	shovelerLogInfo("Connecting as worker %s to %s:%d.", workerId.c_str(), hostname.c_str(), port);
+	worker::Connection connection = worker::Connection::ConnectAsync(hostname, port, workerId, parameters).Get();
+	bool disconnected = false;
+	worker::Dispatcher dispatcher;
+
+	dispatcher.OnDisconnect([&](const worker::DisconnectOp& op) {
+		shovelerLogError("Disconnected from SpatialOS: %s", op.Reason.c_str());
+		disconnected = true;
+	});
+
+
+	game->camera = shovelerCameraIdentityCreate();
+	game->scene = shovelerSceneCreate();
+	game->update = updateGame;
+
+	while(shovelerGameIsRunning(game) && !disconnected) {
+		dispatcher.Process(connection.GetOpList(0));
+		shovelerGameRenderFrame(game);
+	}
+	shovelerLogInfo("Exiting main loop, goodbye.");
+
+	shovelerCameraFree(game->camera);
+	shovelerSceneFree(game->scene);
+
+	shovelerGameFree(game);
+}
+
+static void updateGame(ShovelerGame *game, double dt)
+{
+
 }
