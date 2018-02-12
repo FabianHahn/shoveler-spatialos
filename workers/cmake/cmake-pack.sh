a0bb94d37b9d8a4929c876adf5271a4192824a34
Fabian Hahn <fabian@hahn.graphics>
2018-02-12 22:35:16 +0000

    added server worker

---
+++ workers/cmake/cmake-pack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+uname=`uname`
+if [[ "$uname" == "Linux" ]]; then
+  pushd build/$1
+  cmake -E tar "cfv" "../../../../build/assembly/workers/$2@Linux.zip" --format=zip $2
+  popd
+else
+  pushd build/$1/Release
+  cmake -E tar "cfv" "../../../../../build/assembly/workers/$2@Windows.zip" --format=zip $2.exe
+  popd
+fi
+
