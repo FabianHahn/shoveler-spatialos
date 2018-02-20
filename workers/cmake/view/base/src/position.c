c9870eb9f67ea0e5b623477f7c81e7abd4c10b62
Fabian Hahn <fabian@improbable.io>
2018-02-20 20:19:31 +0000

    added authority and update request support to position component

--- workers/cmake/view/base/src/position.c
+++ workers/cmake/view/base/src/position.c
@@ -1,5 +1,7 @@
 #include <stdlib.h> // malloc free
 
+#include <shoveler/spatialos/log.h>
+
 #include "shoveler/spatialos/worker/view/base/position.h"
 #include "shoveler/spatialos/worker/view/base/view.h"
 
@@ -9,13 +11,13 @@ bool shovelerSpatialosWorkerViewAddEntityPosition(ShovelerSpatialosWorkerView *v
 {
 	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
 	if(entity == NULL) {
-		// shovelerLogWarning("Trying to add position to non existing entity %lld, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to add position to non existing entity %lld, ignoring.", entityId);
 		return false;
 	}
 
 	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
 	if(component != NULL) {
-		// shovelerLogWarning("Trying to add position to entity %lld which already has a position, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to add position to entity %lld which already has a position, ignoring.", entityId);
 		return false;
 	}
 
@@ -23,6 +25,8 @@ bool shovelerSpatialosWorkerViewAddEntityPosition(ShovelerSpatialosWorkerView *v
 	position->x = x;
 	position->y = y;
 	position->z = z;
+	position->requestUpdate = NULL;
+	position->requestUpdateUserData = NULL;
 
 	if (!shovelerSpatialosWorkerViewEntityAddComponent(entity, shovelerSpatialosWorkerViewPositionComponentName, position, &freeComponent)) {
 		freeComponent(component);
@@ -35,13 +39,13 @@ bool shovelerSpatialosWorkerViewUpdateEntityPosition(ShovelerSpatialosWorkerView
 {
 	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
 	if(entity == NULL) {
-		// shovelerLogWarning("Trying to update position for non existing entity %lld, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to update position for non existing entity %lld, ignoring.", entityId);
 		return false;
 	}
 
 	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
 	if(component == NULL) {
-		// shovelerLogWarning("Trying to update position for entity %lld which does not have a position, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to update position for entity %lld which does not have a position, ignoring.", entityId);
 		return false;
 	}
 	ShovelerSpatialosWorkerViewPosition *position = component->data;
@@ -53,17 +57,83 @@ bool shovelerSpatialosWorkerViewUpdateEntityPosition(ShovelerSpatialosWorkerView
 	return shovelerSpatialosWorkerViewEntityUpdateComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
 }
 
-bool shovelerSpatialosWorkerViewRemoveEntityPosition(ShovelerSpatialosWorkerView *view, long long int entityId)
+bool shovelerSpatialosWorkerViewDelegatePosition(ShovelerSpatialosWorkerView *view, long long int entityId, ShovelerSpatialosWorkerViewPositionRequestUpdateFunction *requestUpdateFunction, void *userData)
 {
 	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
 	if(entity == NULL) {
-		// shovelerLogWarning("Trying to remove position from non existing entity %lld, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to delegate position for non existing entity %lld, ignoring.", entityId);
 		return false;
 	}
 
 	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
 	if(component == NULL) {
-		// shovelerLogWarning("Trying to remove position from entity %lld which does not have a position, ignoring.", entityId);
+		shovelerSpatialosLogWarning("Trying to delegate position for entity %lld which does not have a position, ignoring.", entityId);
+		return false;
+	}
+	ShovelerSpatialosWorkerViewPosition *position = component->data;
+
+	position->requestUpdate = requestUpdateFunction;
+	position->requestUpdateUserData = userData;
+
+	return shovelerSpatialosWorkerViewDelegateComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
+}
+
+bool shovelerSpatialosWorkerViewUndelegatePosition(ShovelerSpatialosWorkerView *view, long long int entityId)
+{
+	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
+	if(entity == NULL) {
+		shovelerSpatialosLogWarning("Trying to undelegate position for non existing entity %lld, ignoring.", entityId);
+		return false;
+	}
+
+	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
+	if(component == NULL) {
+		shovelerSpatialosLogWarning("Trying to undelegate position for entity %lld which does not have a position, ignoring.", entityId);
+		return false;
+	}
+	ShovelerSpatialosWorkerViewPosition *position = component->data;
+
+	position->requestUpdate = NULL;
+	position->requestUpdateUserData = NULL;
+
+	return shovelerSpatialosWorkerViewUndelegateComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
+}
+
+bool shovelerSpatialosWorkerViewRequestPositionUpdate(ShovelerSpatialosWorkerView *view, long long int entityId, double x, double y, double z)
+{
+	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
+	if(entity == NULL) {
+		shovelerSpatialosLogWarning("Trying to request position update for non existing entity %lld, ignoring.", entityId);
+		return false;
+	}
+
+	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
+	if(component == NULL) {
+		shovelerSpatialosLogWarning("Trying to request position update for entity %lld which does not have a position, ignoring.", entityId);
+		return false;
+	}
+	ShovelerSpatialosWorkerViewPosition *position = component->data;
+
+	if (!component->authoritative) {
+		shovelerSpatialosLogWarning("Trying to request position update for entity %lld for which this worker is not authoritative, ignoring.", entityId);
+		return false;
+	}
+
+	position->requestUpdate(component, x, y, z, position->requestUpdateUserData);
+	return true;
+}
+
+bool shovelerSpatialosWorkerViewRemoveEntityPosition(ShovelerSpatialosWorkerView *view, long long int entityId)
+{
+	ShovelerSpatialosWorkerViewEntity *entity = shovelerSpatialosWorkerViewGetEntity(view, entityId);
+	if(entity == NULL) {
+		shovelerSpatialosLogWarning("Trying to remove position from non existing entity %lld, ignoring.", entityId);
+		return false;
+	}
+
+	ShovelerSpatialosWorkerViewComponent *component = shovelerSpatialosWorkerViewEntityGetComponent(entity, shovelerSpatialosWorkerViewPositionComponentName);
+	if(component == NULL) {
+		shovelerSpatialosLogWarning("Trying to remove position from entity %lld which does not have a position, ignoring.", entityId);
 		return false;
 	}
 
