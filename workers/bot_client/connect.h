918457cf434c55396a75aa2614d048dbc04504bb
Fabian Hahn <fabian@improbable.io>
2021-01-29 11:51:22 +0000

    reimplemented bot client in C

--- workers/bot_client/connect.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHOVELER_CLIENT_CONNECT_H
-#define SHOVELER_CLIENT_CONNECT_H
-
-#include <improbable/worker.h>
-
-worker::Option<worker::Connection> connect(int argc, char **argv, worker::ConnectionParameters connectionParameters, const worker::ComponentRegistry& components);
-
-#endif
