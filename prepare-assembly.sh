c18436ac5dd2f1feeeda5a00fdddcd75edbc56ac
Fabian Hahn <fabian@improbable.io>
2021-04-23 16:42:05 +0100

    fix paths for live assembly upload v15

--- prepare-assembly.sh
+++ prepare-assembly.sh
@@ -5,17 +5,12 @@ if [ $# -lt 1 ]; then
 	exit 0 
 fi
 
-
 csc BashWrapper.cs
-cp BashWrapper.exe ../shoveler-spatialos-release/build/shoveler-spatialos/build/workers/client/assembly/
+cp BashWrapper.exe build/workers/client/assembly/
 
-cp bot.sh ../shoveler-spatialos-release/build/shoveler-spatialos/build/workers/client/assembly/
+cp bot.sh build/workers/client/assembly/
 
-cp ../shoveler-spatialos-release/build/shoveler-spatialos/build/workers/bot_client/ShovelerBotClient ../shoveler-spatialos-release/build/shoveler-spatialos/build/workers/client/assembly/
-
-pushd ../shoveler-spatialos-release/build/shoveler-spatialos
+cp build/workers/bot_client/ShovelerBotClient build/workers/client/assembly/
 
 spatial alpha cloud upload --assembly_name "shoveler-bots-$1" --environment staging --project_name loom
 spatial project history snapshot upload shoveler build/seeders/tiles.snapshot --tags "shoveler-tiles-$1" --environment staging --project_name loom
-
-popd
