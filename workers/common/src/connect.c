23e37ee740b52ddb27cb25b2412683e2bb033794
Fabian Hahn <fabian@improbable.io>
2021-07-09 11:45:55 +0100

    fixed Locator login

--- workers/common/src/connect.c
+++ workers/common/src/connect.c
@@ -32,13 +32,17 @@ Worker_Connection *shovelerWorkerConnect(int argc, char **argv, int argumentOffs
 
 		gchar **afterDeploymentNameSplit = g_strsplit(afterDeploymentName, "&", 0);
 		GString *authToken = g_string_new("");
+		GString *pit = g_string_new("");
 		const char *locatorHost = "locator.improbable.io";
 		for(int i = 0; afterDeploymentNameSplit[i] != NULL; i++) {
 			const char *tokenPrefix = "token=";
+			const char *pitPrefix = "pit=";
 			const char *environmentPrefix = "environment=";
 
 			if(g_str_has_prefix(afterDeploymentNameSplit[i], tokenPrefix)) {
 				g_string_append(authToken, afterDeploymentNameSplit[i] + strlen(tokenPrefix));
+			} else if(g_str_has_prefix(afterDeploymentNameSplit[i], pitPrefix)) {
+				g_string_append(pit, afterDeploymentNameSplit[i] + strlen(pitPrefix));
 			} else if (g_str_has_prefix(afterDeploymentNameSplit[i], environmentPrefix)) {
 				if (strcmp(afterDeploymentNameSplit[i] + strlen(environmentPrefix), "staging") == 0) {
 					locatorHost = "locator-staging.improbable.io";
@@ -56,11 +60,12 @@ Worker_Connection *shovelerWorkerConnect(int argc, char **argv, int argumentOffs
 			return NULL;
 		}
 
-		shovelerLogInfo("Connecting to cloud deployment...\n\tLocator host: %s\n\tProject name: %s\n\tDeployment name: %s\n\tAuth token: %s", locatorHost, projectName, deploymentName, authToken->str);
+		shovelerLogInfo("Connecting to cloud deployment...\n\tLocator host: %s\n\tProject name: %s\n\tDeployment name: %s\n\tAuth token: %s\n\tPlayer identity token: %s", locatorHost, projectName, deploymentName, authToken->str, pit->str);
 
 		Worker_LocatorParameters locatorParameters;
 		memset(&locatorParameters, 0, sizeof(Worker_LocatorParameters));
 		locatorParameters.player_identity.login_token = authToken->str;
+		locatorParameters.player_identity.player_identity_token = pit->str;
 
 		Worker_Locator *locator = Worker_Locator_Create(locatorHost, /* port */ 0, &locatorParameters);
 
@@ -71,6 +76,7 @@ Worker_Connection *shovelerWorkerConnect(int argc, char **argv, int argumentOffs
 		Worker_ConnectionFuture_Destroy(connectionFuture);
 		Worker_Locator_Destroy(locator);
 		g_string_free(authToken, true);
+		g_string_free(pit, true);
 
 		g_strfreev(projectNameSplit);
 		g_strfreev(deploymentNameSplit);
