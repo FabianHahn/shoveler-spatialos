87f8a756fcff20829f0916bdc1f45b9e77dd88a7
Fabian Hahn <fabian@hahn.graphics>
2021-11-13 19:32:01 +0000

    Squashed 'shoveler/' changes from 2379c624b..5b0bae612
    5b0bae612 updated fakeglib to 14112c08f224b99f22ab83201334faab5fd13a65
    1dcb8ec19 Squashed 'thirdparty/fakeglib/' changes from 3d64a942..14112c08
    9b5e4c9ee ported view example to client
    cfec11b09 added sprite component to client system
    bfbe308ee added tilemap sprite component to client system
    e15f912c7 added texture sprite component to client system
    6749a7c6d added text sprite component to client system
    1ac8cf48a added tile sprite animation component to client system
    abd2e2853 added tile sprite component to client system
    452ade878 added client component to client system
    f7a22e55b added model component to client system
    4b7a78499 added material component to client system
    0f720c469 added tilemap component to client system
    2f1a7aff3 added tilemap tiles component to client system
    d43dc7d9f added tilemap colliders component to client system
    6e404b3ed added tileset component to client system
    866af99c6 added light component to client system
    c030a2dc4 added texture component to client system
    fae45117e added text texture renderer component to client system
    dc20484a4 added font atlas texture component to component system
    c46821272 added sampler component to client system
    a1fea5718 added drawable component to client system
    a866652d3 added canvas component to client system
    4b2f91b0d added font atlas component to client system
    90a25d277 added font component to client system
    701a4083b added position component to client system
    fe33ff244 added image component to client system
    3d396d864 added resource component to client system
    17b900e34 extracted view into separate view_base and view_opengl libraries
    d0080c1c7 split game view out of game
    2726ab741 added client system
    4cd2b1850 allow forward declaration of executor callbacks
    77e583f2f allow forward declarations of input callbacks
    95178b6af added opengl schema
    33c1927a8 added base schema
    c42ddca5d added world dependency graph
    5a3eaea10 added world
    fd78d31a2 added system
    e29c73f9e added component system
    21a1393a1 added schema
    c867b5736 added entity component ID
    ea32532bd removed base component data
    d31814ffd added ecs component
    16b76b54e added ecs component type
    47b470fe9 added ecs component field
    f1d89bd53 added ecs library
    e3396cd2c added .clang-format
    
    git-subtree-dir: shoveler
    git-subtree-split: 5b0bae612bbcfdc87ba8e00c58090214461a787b

---
+++ client/src/client_system.c
@@ -0,0 +1,178 @@
+#include "shoveler/client_system.h"
+
+#include <glib.h>
+#include <stdlib.h>
+
+#include "shoveler/colliders.h"
+#include "shoveler/component/canvas.h"
+#include "shoveler/component/client.h"
+#include "shoveler/component/drawable.h"
+#include "shoveler/component/font.h"
+#include "shoveler/component/font_atlas.h"
+#include "shoveler/component/font_atlas_texture.h"
+#include "shoveler/component/image.h"
+#include "shoveler/component/light.h"
+#include "shoveler/component/material.h"
+#include "shoveler/component/model.h"
+#include "shoveler/component/position.h"
+#include "shoveler/component/resource.h"
+#include "shoveler/component/sampler.h"
+#include "shoveler/component/sprite.h"
+#include "shoveler/component/text_sprite.h"
+#include "shoveler/component/text_texture_renderer.h"
+#include "shoveler/component/texture.h"
+#include "shoveler/component/texture_sprite.h"
+#include "shoveler/component/tile_sprite.h"
+#include "shoveler/component/tile_sprite_animation.h"
+#include "shoveler/component/tilemap.h"
+#include "shoveler/component/tilemap_colliders.h"
+#include "shoveler/component/tilemap_sprite.h"
+#include "shoveler/component/tilemap_tiles.h"
+#include "shoveler/component/tileset.h"
+#include "shoveler/controller.h"
+#include "shoveler/executor.h"
+#include "shoveler/font.h"
+#include "shoveler/game.h"
+#include "shoveler/input.h"
+#include "shoveler/log.h"
+#include "shoveler/render_state.h"
+#include "shoveler/scene.h"
+#include "shoveler/schema.h"
+#include "shoveler/schema/base.h"
+#include "shoveler/schema/opengl.h"
+#include "shoveler/shader_cache.h"
+#include "shoveler/system.h"
+#include "shoveler/world.h"
+#include "shoveler/world_dependency_graph.h"
+
+static void clientSystemUpdateAuthoritativeComponent(
+    ShovelerWorld* world,
+    ShovelerComponent* component,
+    const ShovelerComponentField* field,
+    const ShovelerComponentFieldValue* value,
+    void* clientSystemPointer);
+static void clientSystemKeyHandler(
+    ShovelerInput* input, int key, int scancode, int action, int mods, void* clientSystemPointer);
+static void clientSystemUpdateWorldCounters(void* clientSystemPointer);
+
+ShovelerClientSystem* shovelerClientSystemCreate(
+    ShovelerGame* game,
+    ShovelerClientSystemUpdateAuthoritativeComponentFunction* updateAuthoritativeComponent,
+    void* updateAuthoritativeComponentUserData) {
+  ShovelerClientSystem* clientSystem = malloc(sizeof(ShovelerClientSystem));
+  clientSystem->system = shovelerSystemCreate();
+  clientSystem->schema = shovelerSchemaCreate();
+  clientSystem->world = shovelerWorldCreate(
+      clientSystem->schema,
+      clientSystem->system,
+      clientSystemUpdateAuthoritativeComponent,
+      clientSystem);
+  clientSystem->executor = game->updateExecutor;
+  clientSystem->input = game->input;
+  clientSystem->colliders = game->colliders;
+  clientSystem->fonts = game->fonts;
+  clientSystem->controller = game->controller;
+  clientSystem->shaderCache = game->shaderCache;
+  clientSystem->scene = game->scene;
+  clientSystem->renderState = &game->renderState;
+  clientSystem->updateAuthoritativeComponent = updateAuthoritativeComponent;
+  clientSystem->updateAuthoritativeComponentUserData = updateAuthoritativeComponentUserData;
+  clientSystem->keyCallback =
+      shovelerInputAddKeyCallback(game->input, clientSystemKeyHandler, clientSystem);
+  clientSystem->updateWorldCountersExecutorCallback = shovelerExecutorSchedulePeriodic(
+      clientSystem->executor,
+      /* timeoutMs */ 0,
+      /* intervalMs */ 1000,
+      clientSystemUpdateWorldCounters,
+      clientSystem);
+  clientSystem->lastWorldCounters.numEntities = 0;
+  clientSystem->lastWorldCounters.numComponents = 0;
+  clientSystem->lastWorldCounters.numComponentDependencies = 0;
+
+  shovelerSchemaBaseRegister(clientSystem->schema);
+  shovelerSchemaOpenglRegister(clientSystem->schema);
+
+  shovelerClientSystemAddCanvasSystem(clientSystem);
+  shovelerClientSystemAddClientSystem(clientSystem);
+  shovelerClientSystemAddDrawableSystem(clientSystem);
+  shovelerClientSystemAddFontSystem(clientSystem);
+  shovelerClientSystemAddFontAtlasSystem(clientSystem);
+  shovelerClientSystemAddFontAtlasTextureSystem(clientSystem);
+  shovelerClientSystemAddImageSystem(clientSystem);
+  shovelerClientSystemAddLightSystem(clientSystem);
+  shovelerClientSystemAddMaterialSystem(clientSystem);
+  shovelerClientSystemAddModelSystem(clientSystem);
+  shovelerClientSystemAddPositionSystem(clientSystem);
+  shovelerClientSystemAddSamplerSystem(clientSystem);
+  shovelerClientSystemAddSpriteSystem(clientSystem);
+  shovelerClientSystemAddTextSpriteSystem(clientSystem);
+  shovelerClientSystemAddTextTextureRendererSystem(clientSystem);
+  shovelerClientSystemAddTextureSystem(clientSystem);
+  shovelerClientSystemAddTextureSpriteSystem(clientSystem);
+  shovelerClientSystemAddTileSpriteSystem(clientSystem);
+  shovelerClientSystemAddTileSpriteAnimationSystem(clientSystem);
+  shovelerClientSystemAddTilemapSystem(clientSystem);
+  shovelerClientSystemAddTilemapCollidersSystem(clientSystem);
+  shovelerClientSystemAddTilemapSpriteSystem(clientSystem);
+  shovelerClientSystemAddTilemapTilesSystem(clientSystem);
+  shovelerClientSystemAddTilesetSystem(clientSystem);
+
+  return clientSystem;
+}
+
+void shovelerClientSystemFree(ShovelerClientSystem* clientSystem) {
+  shovelerExecutorRemoveCallback(
+      clientSystem->executor, clientSystem->updateWorldCountersExecutorCallback);
+  shovelerInputRemoveKeyCallback(clientSystem->input, clientSystem->keyCallback);
+  shovelerWorldFree(clientSystem->world);
+  shovelerSchemaFree(clientSystem->schema);
+  shovelerSystemFree(clientSystem->system);
+  free(clientSystem);
+}
+
+static void clientSystemUpdateAuthoritativeComponent(
+    ShovelerWorld* world,
+    ShovelerComponent* component,
+    const ShovelerComponentField* field,
+    const ShovelerComponentFieldValue* value,
+    void* clientSystemPointer) {
+  ShovelerClientSystem* clientSystem = clientSystemPointer;
+
+  if (clientSystem->updateAuthoritativeComponent != NULL) {
+    clientSystem->updateAuthoritativeComponent(
+        clientSystem, component, field, value, clientSystem->updateAuthoritativeComponentUserData);
+  }
+}
+
+static void clientSystemKeyHandler(
+    ShovelerInput* input, int key, int scancode, int action, int mods, void* clientSystemPointer) {
+  ShovelerClientSystem* clientSystem = clientSystemPointer;
+
+  if (key == GLFW_KEY_F9 && action == GLFW_PRESS) {
+    shovelerLogInfo("F9 key pressed, writing world dependency graph.");
+    shovelerWorldDependencyGraphWrite(clientSystem->world, "world_dependencies.dot");
+  }
+}
+
+static void clientSystemUpdateWorldCounters(void* clientSystemPointer) {
+  ShovelerClientSystem* clientSystem = clientSystemPointer;
+
+  bool worldCountersChanged = clientSystem->lastWorldCounters.numEntities !=
+          g_hash_table_size(clientSystem->world->entities) ||
+      clientSystem->lastWorldCounters.numComponents != clientSystem->world->numComponents ||
+      clientSystem->lastWorldCounters.numComponentDependencies !=
+          clientSystem->world->numComponentDependencies;
+
+  if (worldCountersChanged) {
+    clientSystem->lastWorldCounters.numEntities = g_hash_table_size(clientSystem->world->entities);
+    clientSystem->lastWorldCounters.numComponents = clientSystem->world->numComponents;
+    clientSystem->lastWorldCounters.numComponentDependencies =
+        clientSystem->world->numComponentDependencies;
+
+    shovelerLogInfo(
+        "World changed: %u entities, %u components, %u dependencies.",
+        clientSystem->lastWorldCounters.numEntities,
+        clientSystem->lastWorldCounters.numComponents,
+        clientSystem->lastWorldCounters.numComponentDependencies);
+  }
+}
